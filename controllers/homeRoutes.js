f86ea3af0b6f0142b323c9175bdf096c254e05d7
Mostafa Maki <mostafa_m9@yahoo.com>
2022-10-20 01:25:33 -0700

    fixed some bugs

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,7 +27,10 @@ router.get('/home', withAuth, async (req, res) => {
         const characters = characterData.map((character)=> character.get({ plain: true }))
         const serializedData = userData.get({ plain: true });
         //this is where i fix the icon
+        console.log("-----------icon fix-------")
         console.log(serializedData.charUsers)
+        console.log("-----------icon fix-------")
+        console.log(serializedData)
         icon(serializedData.name);
         res.render('home', {
             serializedData,
@@ -76,23 +79,28 @@ router.get('/lottery', withAuth, async (req, res) => {
     })
 });
 
-
 router.get('/archive', withAuth, async (req, res) => {
   try {
-      const userData = await Character.findAll();
-
+      const charData = await Character.findAll();
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model:CharUser, include: [Character]}],
+      }) 
       if (!userData) {
         res
           .status(400)
           .json({ message: 'Error! Please try again!' });
         return;
       }
-      const characters = userData.map((character) => character.get( { plain: true }));
-
+      const serializedData = userData.get({ plain: true });
+      const characters = charData.map((character) => character.get( { plain: true }));
+      console.log("-----------icon fix in archive-------")
       console.log(characters)
-  
+      console.log("-----------icon fix in archive-------")
+      console.log(serializedData)
       res.render('archive', {
         characters,
+        serializedData,
         logged_in: req.session.logged_in
       })
     } catch (err) {
